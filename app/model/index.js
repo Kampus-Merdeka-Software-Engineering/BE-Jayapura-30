a556a023c242495d67222fdedd4cbde9f67d91b0
AqiilahNurAzzyyati <qilaazzyyatii@gmail.com>
2023-10-09 12:58:34 +0800

    update

--- app/model/index.js
+++ app/model/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const fs = require('fs')
 const path = require('path')
 
-const conn = new Sequelize("mysql://avnadmin:AVNS_QUDKyLHvQFQMuqTcc2h@mysql-365e68a1-capstoneproject-group30.aivencloud.com:19317/defaultdb?ssl-mode=REQUIRED", {
+const conn = new Sequelize("mysql://avnadmin:AVNS_QUDKyLHvQFQMuqTcc2h@mysql-365e68a1-capstoneproject-group30.aivencloud.com:19317/login_system?ssl-mode=REQUIRED", {
     ssl: fs.readFileSync(path.join(__dirname,'ca.pem')),
     dialect: 'mysql',
     logging: false
