aa79f7998067bf2569a6389c9b767c50173a25ae
AqiilahNurAzzyyati <qilaazzyyatii@gmail.com>
2023-10-09 10:11:19 +0800

    fix path

--- app/model/index.js
+++ app/model/index.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 const mysql = require('mysql2')
 const fs = require('fs')
-const path = require('path')
+const pathtoCaPem = ('./app/model/ca.pem')
 
 const conn = new Sequelize("mysql://avnadmin:AVNS_QUDKyLHvQFQMuqTcc2h@mysql-365e68a1-capstoneproject-group30.aivencloud.com:19317/defaultdb?ssl-mode=REQUIRED", {
-    ssl: fs.readFileSync(path.join(__dirname,'ca.pem')),
+    ssl: fs.readFileSync(pathtoCaPem),
     dialect: 'mysql',
     logging: false
 });
